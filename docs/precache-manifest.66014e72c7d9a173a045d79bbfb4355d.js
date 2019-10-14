9f8359332e088c1e789af5d952fc9776abfc6535
maheshoz <maheshcn31@gmail.com>
2019-10-14 13:23:14 +0530

    added docs folder

---
+++ docs/precache-manifest.66014e72c7d9a173a045d79bbfb4355d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4e4b93ea1aacfb33272c7569c02a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174b5e52353b4361ab24",
+    "url": "/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "6da700e7c14399c7fc77",
+    "url": "/static/css/main.db278f76.chunk.css"
+  },
+  {
+    "revision": "174b5e52353b4361ab24",
+    "url": "/static/js/2.3dfe6570.chunk.js"
+  },
+  {
+    "revision": "6da700e7c14399c7fc77",
+    "url": "/static/js/main.3bfba035.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  }
+]);
